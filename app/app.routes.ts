fea1d7e47c3e794ec9a5be0060309f93a4e91895
gabrielgio <gabrielgio@localhost.localdomain>
2017-01-28 23:10:33 -0200

    Adds component for connections form

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,10 +1,12 @@
 import {Routes, RouterModule} from '@angular/router'
 import {CodemirrorComponent} from './codemirror/codemirror.component'
 import {ConComponent} from './connections/con.component'
+import {FormComponent} from './form/form.component'
 
 const appRoutes: Routes = [
     {path: '', component: ConComponent},
     {path: 'shell', component: CodemirrorComponent},
+    {path: 'add', component: FormComponent},
     {path: '**', component: ConComponent}
 ]
 
--- app/connections/con.module.ts
+++ app/connections/con.module.ts
@@ -1,9 +1,12 @@
 import {NgModule} from '@angular/core'
 import {ConComponent} from './con.component'
+import {ConnectionService} from '../Service/ConnectionService'
 
 @NgModule({
     declarations: [ConComponent],
-    bootstrap: [ConComponent]
+    bootstrap: [ConComponent],
+    providers: [ConnectionService]
 })
 export class ConModule {
+
 }
