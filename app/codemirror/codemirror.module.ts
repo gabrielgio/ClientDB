7554874e637f3453faf5fd580b4d835ad6dc6e57
gabrielgio <gabrielg.desouza@gmail.com>
2017-02-04 15:27:57 -0200

    From Scratch

--- app/codemirror/codemirror.module.ts
+++
@@ -1,9 +0,0 @@
-import {NgModule} from '@angular/core'
-import {CodemirrorComponent} from './codemirror.component'
-
-@NgModule({
-    declarations: [CodemirrorComponent],
-    bootstrap: [CodemirrorComponent]
-})
-export class CodemirrorModule {
-}
